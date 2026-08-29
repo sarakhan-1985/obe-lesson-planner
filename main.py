361d05d43ba02337df211f785fff38edef14044d
Sara Khan <sara.khan1@nu.edu.pk>
2026-08-29 06:01:21 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1895,13 +1895,4 @@ ui.run(
 )
 
 
-# =========================================================
-# RUN APP
-# =========================================================
 
-ui.run(
-    title='OBE Lesson Planning Assistant',
-    host='0.0.0.0',
-    port=int(os.environ.get('PORT', 8080)),
-    reload=False
-)
